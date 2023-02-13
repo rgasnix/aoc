99abd6c47de925911a4b24e5c6adc13a9d3f923a
Guilherme A. de Souza <rgasnix@gmail.com>
2023-02-13 12:53:21 -0300

    fix: Changed the input.txt files, fixed indentation

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -8,20 +8,20 @@ fn main() {
     let mut sum: i32 = 0;
 
     for value in vec_input.iter() {
-	if value.is_empty() {
-	    elfs.push(sum);
-	    sum = 0;
-	} else {
-	    sum += match value.trim().parse::<i32>() {
-		Ok(n) => n,
-		Err(_) => 0,
-	    };
-	}
+	    if value.is_empty() {
+	        elfs.push(sum);
+	        sum = 0;
+	    } else {
+	        sum += match value.trim().parse::<i32>() {
+		        Ok(n) => n,
+		        Err(_) => 0,
+	        };
+	    }
     }
 
     elfs.sort();
 
-    println!("{0}", elfs[elfs.len()-1]);
+    println!("Score: {0}", elfs[elfs.len()-1]);
 }
 
 

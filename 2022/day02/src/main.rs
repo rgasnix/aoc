18c9fe1bad5684809ae966da3b5fc2ba11f75512
Guilherme A. de Souza <rgasnix@gmail.com>
2023-02-15 00:46:07 -0300

    update: Day 02 Part 2 Complete

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -3,26 +3,40 @@ use std::fs;
 fn main() {
     let input: String = read_input("input.txt");
     let vec_input: Vec<&str> = input.split("\n").collect();
-    let mut score: i32 = 0;
+    let mut score_part_one: i32 = 0;
+    let mut score_part_two: i32 = 0;
 
     for i in vec_input.iter() {
 	    if !i.is_empty() {
 	        match i {
-		        &"A X" => score += 4,
-		        &"A Y" => score += 8,
-		        &"A Z" => score += 3,
-		        &"B X" => score += 1,
-		        &"B Y" => score += 5,
-		        &"B Z" => score += 9,
-		        &"C X" => score += 7,
-		        &"C Y" => score += 2,
-		        &"C Z" => score += 6,
-		        &_ => score += 0,
+		        &"A X" => score_part_one += 4,
+		        &"A Y" => score_part_one += 8,
+		        &"A Z" => score_part_one += 3,
+		        &"B X" => score_part_one += 1,
+		        &"B Y" => score_part_one += 5,
+		        &"B Z" => score_part_one += 9,
+		        &"C X" => score_part_one += 7,
+		        &"C Y" => score_part_one += 2,
+		        &"C Z" => score_part_one += 6,
+		        &_ => score_part_one += 0,
 	        }
+            match i {   
+                &"A X" => score_part_two += 3,
+		        &"A Y" => score_part_two += 4,
+		        &"A Z" => score_part_two += 8,
+		        &"B X" => score_part_two += 1,
+		        &"B Y" => score_part_two += 5,
+		        &"B Z" => score_part_two += 9,
+		        &"C X" => score_part_two += 2,
+		        &"C Y" => score_part_two+= 6,
+		        &"C Z" => score_part_two += 7,
+		        &_ => score_part_two += 0,
+            }
 	    }
     }
 
-    println!("Score: {0}", score);
+    println!("Score Part One: {0}", score_part_one);
+    println!("Score Part Two: {0}", score_part_two);
 }
 
 fn read_input(file_path: &str) -> String {
